e8589cf6c3b2b79887f8c597980b2adc285e6721
mercy <merzsg1@gmail.com>
2019-12-30 12:56:40 -0600

    endpoint con GET y params

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false})) //se le agregan los middelware
 app.use(bodyParser.json())
 
+app.get('/hola/:name', (req, res) => {
+    res.send({message: `Hola ${req.params.name}`}) //Get con parametros
+})
+
+
 app.listen(port, () => {
     console.log(`API REST corriendo en http://localhost:${port}`)
 })
